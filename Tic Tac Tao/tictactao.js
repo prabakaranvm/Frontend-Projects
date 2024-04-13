3cd8fae31d8b476d5f639feb152fe1bdeb267f9d
prabakaranvm <prabakaranvm1301@gmail.com>
2024-04-13 14:58:54 +0530

    Changes Completed

--- Tic Tac Tao/tictactao.js
+++ Tic Tac Tao/tictactao.js
@@ -1,4 +1,11 @@
 //DOM Declaration
+//Welcome
+let welcomeDom = document.getElementById("welcomeId")
+let startBtnDom = document.getElementById("startBtn")
+let player1Dom = document.getElementById("player1")
+let player2Dom = document.getElementById("player2")
+//Game
+let imgBgDom = document.getElementById("imgBgId")
 let cellDom = document.querySelectorAll(".cell")
 let displayDom = document.getElementById("displayId")
 let resultDom = document.getElementById("resultId")
@@ -13,75 +20,90 @@ let cellBoard = ["", "", "", "", "", "", "", "", ""]
 let currentPlayer = "X"
 let gameRunning = true
 
-cellDom.forEach(cell => cell.addEventListener("click", clickedCell))
-restartDom.addEventListener("click", restart)
-startGame()
-//Functions
-function startGame() 
-{
-    displayDom.textContent = "Player X's turn"
-}
-function clickedCell() 
-{
-    const index = parseInt(this.getAttribute("index"))
-    if (cellBoard[index] === "" && gameRunning == true) 
-    {
-        updateCell(index)
-        checkWinner()
+//Welcome Page
+startBtnDom.addEventListener("click", () => {
+    let getPlayerName1 = player1Dom.value
+    let getPlayerName2 = player2Dom.value
+    if (getPlayerName1 !== "" && getPlayerName2 !== "") {
+        welcomeDom.style.display = "none"
+        imgBgDom.style.display = "block"
+        gameStart(getPlayerName1, getPlayerName2)
     }
-}
-function updateCell(index) 
-{
-    cellBoard[index] = currentPlayer
-    cellDom[index].textContent = currentPlayer
-}
-function changePlayer() 
-{
-    currentPlayer = (currentPlayer === "X" ) ? "O" : "X"
-    displayDom.textContent = "Player " + currentPlayer + "'s turn"
-}
-function checkWinner() 
-{
-    let roundWon = false
-   for(let start = 0; start<wins.length; start++)
-   {
-        const check = wins[start]
-        const cellOne = cellBoard[check[0]]
-        const cellTwo = cellBoard[check[1]]
-        const cellThree = cellBoard[check[2]]
-        if(cellOne == "" || cellTwo == "" || cellThree == "")
-        {
-            continue    
-        }
-        if(cellOne == cellTwo && cellTwo == cellThree)
-        {
-            roundWon = true
-            break
-        }
-   }
-   if(roundWon)
-   {
-    resultDom.textContent = "Player " + currentPlayer + " wins"
-    displayDom.textContent = ""
-    gameRunning = false
-   }
-   else if(!cellBoard.includes(""))
-   {
-    resultDom.textContent = "Match Draw"
-    gameRunning = false
-   }
-   else
-   {
-    changePlayer()
-   }
-}
-function restart() {
-    cellBoard = ["", "", "", "", "", "", "", "", ""]
-    cellDom.forEach(cell => cell.textContent = "")
-    resultDom.textContent = ""
-    currentPlayer = "X"
-    gameRunning = true
-    startGame()
-}
+    else {
+        alert("Please enter palyer names")
+    }
+})
+function gameStart(playerName1, playerName2) {
 
+    //Game Page
+    cellDom.forEach(cell => cell.addEventListener("click", clickedCell))
+    restartDom.addEventListener("click", restart)
+    startGame()
+    //Functions
+    function startGame() {
+        displayDom.textContent = `It's ${playerName1}'s turn`
+    }
+    function clickedCell() {
+        const index = parseInt(this.getAttribute("index"))
+        if (cellBoard[index] === "" && gameRunning == true) {
+            updateCell(index)
+            checkWinner()
+        }
+    }
+    function updateCell(index) {
+        cellBoard[index] = currentPlayer
+        cellDom[index].textContent = currentPlayer
+    }
+    function changePlayer() {
+        currentPlayer = (currentPlayer === "X") ? "O" : "X"
+        displayDom.textContent = "It's " + (currentPlayer === "X" ? playerName1 : playerName2) + "'s turn";
+    }
+    function checkWinner() {
+        let roundWon = false
+        for (let start = 0; start < wins.length; start++) {
+            const check = wins[start]
+            const cellOne = cellBoard[check[0]]
+            const cellTwo = cellBoard[check[1]]
+            const cellThree = cellBoard[check[2]]
+            if (cellOne == "" || cellTwo == "" || cellThree == "") {
+                continue
+            }
+            if (cellOne == cellTwo && cellTwo == cellThree) {
+                roundWon = true
+                break
+            }
+        }
+        if (roundWon) {
+            resultDom.textContent = (currentPlayer === "X" ? playerName1 : playerName2) + " wins"
+            displayDom.textContent = ""
+            gameRunning = false
+            cellDom.forEach((cell, index) => {
+                if (wins.some(combination => combination.includes(index))) {
+                    cell.style.backgroundColor = (cellBoard[index] === currentPlayer) ? '#4CAF50' : '#FF5252'
+                    cell.style.color = 'white'
+                }
+            });
+        }
+        else if (!cellBoard.includes("")) {
+            resultDom.textContent = "Match Draw"
+            gameRunning = false
+        }
+        else {
+            changePlayer()
+        }
+    }
+    function restart() {
+        cellBoard = ["", "", "", "", "", "", "", "", ""]
+        cellDom.forEach(cell => {
+            cell.textContent = ""
+            cell.style.backgroundColor = ''
+            cell.style.color = ''
+        })
+        resultDom.textContent = ""
+        currentPlayer = "X"
+        gameRunning = true
+        startGame()
+    }
+
+}
 
